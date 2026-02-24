7ae66d3f4cdda923f8e8c4ee7cfd8838eba657a2
javkhlantS <javkhlant.altankhuyag@gmail.com>
2026-02-24 19:16:21 +0800

    add social login buttons to signin and signup screens

--- lib/features/auth/widgets/social_login.dart
+++ lib/features/auth/widgets/social_login.dart
@@ -1,7 +1,9 @@
 import 'package:appoinx/core/theme/extensions/app_colors_extensions.dart';
 import 'package:appoinx/core/theme/extensions/app_text_styles_extensions.dart';
+import 'package:appoinx/core/utils/platform_utils.dart';
 import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SocialLogin extends StatelessWidget {
   const SocialLogin({super.key});
@@ -34,7 +36,70 @@ class SocialLogin extends StatelessWidget {
           ],
         ),
         const SizedBox(height: 28),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 16,
+          children: [
+            _SocialButton(
+              logo: "assets/svg/google.svg",
+              onTap: () {},
+            ),
+            if (isIOS())
+              _SocialButton(
+                logo: "assets/svg/apple.svg",
+                onTap: () {},
+              ),
+          ],
+        ),
+        const SizedBox(height: 16),
       ],
+    );
+  }
+}
+
+class _SocialButton extends StatelessWidget {
+  final String logo;
+  final VoidCallback onTap;
+
+  const _SocialButton({
+    required this.logo,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: context.appColorsExtensions.neutral40,
+          width: 1,
+        ),
+        shape: BoxShape.circle,
+      ),
+      child: ClipOval(
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            splashColor: context.appColorsExtensions.green300.withValues(
+              alpha: 0.3,
+            ),
+            highlightColor: context.appColorsExtensions.green300.withValues(
+              alpha: 0.2,
+            ),
+            child: Center(
+              child: SvgPicture.asset(
+                logo,
+                width: 24,
+                height: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
